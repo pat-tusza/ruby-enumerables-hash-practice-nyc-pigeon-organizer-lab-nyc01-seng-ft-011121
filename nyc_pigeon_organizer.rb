da8045c16f4c725112ef08f352cf8e2b7f17fe5e
Patrick Tuszakowski <mrtall500@gmail.com>
2020-12-30 17:12:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_new = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+
+        if !pigeon_new[name]
+          pigeon_new[name] = {}
+        end
+
+        if !pigeon_new[name][key]
+          pigeon_new[name][key] = []
+        end
+
+        pigeon_new[name][key] << new_value.to.s
+
+      end
+    end
+  end
+  
+    
 end
